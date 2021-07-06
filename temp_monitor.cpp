129e9f23cc06fc9fa01e45d1ac15866fa71e6978
Daniel Ramirez <louisdra8@protonmail.com>
2021-07-05 23:14:22 -0400

    First checkpoint

--- temp_monitor.cpp
+++ temp_monitor.cpp
@@ -1,3 +1,10 @@
-#include <iostream>
+#include "config_file_parser.h"
 
+
+using namespace std;
+
+int main (int argc, char* argv[]) {
+
+    return 0;
+}
 
---
+++ temp_monitor.h
@@ -0,0 +1,10 @@
+#ifndef TEMP_MONITOR
+#define TEMP_MONITOR
+
+static int highTempLimit;   // In Celsius
+static int lowTempLimit;    // In Celsius
+static long loggingPeriod;  // In seconds
+
+
+
+#endif
