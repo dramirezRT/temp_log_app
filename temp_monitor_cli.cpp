e8a09ce62e41988ad6dff028f054339ce3604ffa
Daniel Ramirez <louisdra8@protonmail.com>
2021-07-09 13:28:55 -0600

    Welcome message update

--- temp_monitor_cli.cpp
+++ temp_monitor_cli.cpp
@@ -41,6 +41,7 @@ int main (int argc, char* argv[]) {
     } else if (arg == "-w" || arg == "--display-welcome")
     {
         cout << rimacWelcome << endl;
+        cout << "From CR with love <3" << endl;
     } else if (arg == "-d" || arg == "--display-config")
     {
         shared_ptr<System> mySystem = cfh.parse_config_file();
